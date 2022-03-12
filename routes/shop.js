2e2cd0d00952c0e759a4bbb9257349c1c5039ccb
ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-03-12 20:44:43 +0100

    remove a fil

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const {products} =require("./admin")
 
 router.get('/', (req, res, next) => {
   console.log(products)
-  res.status(200).render("shop",{prod:products, title:"My Shopping",prodTitle:"Great Books"})
+  res.status(200).render("shop",{prod:products, title:"My Shopping",prodTitle:"Great Books",path:"/"})
 });
 
 module.exports = router;
